c0ac0f7dab00c1a94bef5da795ad553fea2f9a30
Kiefer Levine <kiefer_8@hotmail.com>
2021-05-19 13:04:42 -0400

    Day now populates using moment.js, with colours for past, present and future hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,34 @@
 const containerEl = $(".container");
 const dateEl = $("#currentDay");
 
-// Generates the day on the top of the page
-const displayDay = () => dateEl.text(moment().format("MMM Do, YYYY"));
+// The current date and time
+let currentTime = moment().format("LT");
+let currentDay = moment();
 
-const createTimeBlocks = () => {
+// Keeps the date and time accurate to within a half an hour
+const updateCurrentTime = setInterval(function() {
+    currentTime = currentDay.format("LT");
+    currentDay = moment();
+    displayDay();
+    console.log(currentTime);
+}, (1000 * 60) * 30);
+
+// Generates the day on the top of the page
+const displayDay = () => dateEl.text(currentDay.format("MMM Do, YYYY"));
+
+// If this function returns 1, the current time is before the hour block. If it returns 0, the current time is within the hour of the hour block. If it returns -1, the current time is after the hour of the hour block. 
+const compareHours = (hourEl) => {
+    const hour = hourEl.attr("id");
+    const parsedHour = hour.slice(4, 6);
+
+    const newTime = moment(currentDay).hour(parsedHour);
+
+    if (moment(currentDay).isBefore(newTime)) return 1;
+    else if (moment(currentDay).isAfter(newTime)) return -1;
+    return 0;
+};
+
+const createHourBlocks = () => {
     const dayBlockEl = $("<div>");
 
     dayBlockEl.addClass("column-12");
@@ -12,27 +36,30 @@ const createTimeBlocks = () => {
     containerEl.append(dayBlockEl);
 
     // The hours are each looped through
-    for (let i = 0 ; i < 25 ; i++) {
+    for (let i = 9 ; i < 18 ; i++) {
         const hourBlockEl = $("<div>");
         const hourEl = $("<h6>");
         const descriptionEl = $("<div>");
         const saveBtnEl = $("<button>");
-
         const saveIconEl = $("<span>");
 
         hourBlockEl.addClass("row");
 
         hourEl.addClass("hour col-1 pt-3");
-        descriptionEl.addClass("description col-10 past");
+        descriptionEl.addClass("description col-9");
         saveBtnEl.addClass("saveBtn");
-        saveIconEl.addClass("oi oi-locked");
+        saveIconEl.addClass("oi oi-lock-locked");
+        // Determine the hour via moment.js both for display and variable purposes
+        const hour = moment({hour: i});
+        hourEl.text(hour.format("hA"));  
+        hourEl.attr("id", "hour" + hour.format("kk"));  
 
-        if (!i) {
-            hourEl.text("12AM");
-        } else if (i < 13) {
-            hourEl.text(i + "AM");
-        } else {
-            hourEl.text((i - 12) + "PM");
+        if (compareHours(hourEl) === 1) {
+            descriptionEl.addClass("future");
+        } else if (compareHours(hourEl) === -1) {
+            descriptionEl.addClass("past");
+        } else if (compareHours(hourEl) === 0) {
+            descriptionEl.addClass("present");
         }
 
         saveBtnEl.append(saveIconEl);
@@ -46,4 +73,4 @@ const createTimeBlocks = () => {
 };
 
 displayDay();
-createTimeBlocks();
+createHourBlocks();
