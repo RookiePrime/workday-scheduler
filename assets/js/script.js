a24ac090c7327c67e97eb7455b20f6134d7c3038
Kiefer Levine <kiefer_8@hotmail.com>
2021-05-22 01:06:18 -0400

    User can now add text to the page in each hour block

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ const compareHours = (hourEl) => {
     const parsedHour = hour.slice(4, 6);
 
     const newTime = moment(currentDay).hour(parsedHour);
-
+    // TODO: change this to be an a-b return, could potentially shrink this function way down
     if (moment(currentDay).isBefore(newTime)) return 1;
     else if (moment(currentDay).isAfter(newTime)) return -1;
     return 0;
@@ -46,7 +46,7 @@ const createHourBlocks = () => {
         hourBlockEl.addClass("row");
 
         hourEl.addClass("hour col-1 pt-3");
-        descriptionEl.addClass("description col-9");
+        descriptionEl.addClass("description col-9 pt-2");
         saveBtnEl.addClass("saveBtn");
         saveIconEl.addClass("oi oi-lock-locked");
         // Determine the hour via moment.js both for display and variable purposes
@@ -71,6 +71,62 @@ const createHourBlocks = () => {
         dayBlockEl.append(hourBlockEl);
     }
 };
+
+const startEditing = (descriptionEl) => {
+    const classes = descriptionEl.attr("class");
+    const contents = descriptionEl.text().trim();
+    
+    // Create the textarea for input, with fixings
+    const textareaEl = $("<textarea>");
+    textareaEl.addClass(classes)
+    .text(contents);
+    // Change lock icon in button to unlock icon
+    const saveBtnEl = descriptionEl.next(".saveBtn");
+    const spanEl = saveBtnEl.children("span");
+    spanEl.removeClass("oi-lock-locked")
+        .addClass("oi-lock-unlocked");
+    // Swap it in and make it the focus so the user can tell they can change the text now
+    descriptionEl.replaceWith(textareaEl);
+    textareaEl.trigger("focus");
+};
+
+const finishEditing = (descriptionEl) => {
+    const classes = descriptionEl.attr("class");
+    const contents = descriptionEl.val().trim();
+    
+    // Create the div for the saved description, with fixings
+    const divEl = $("<div>");
+    divEl.addClass(classes)
+    .text(contents);
+    // Change unlock icon in button to lock icon
+    const saveBtnEl = descriptionEl.next(".saveBtn");
+    const spanEl = saveBtnEl.children("span");
+    spanEl.removeClass("oi-lock-unlocked")
+    .addClass("oi-lock-locked");
+    // Swap in the div in place of the textarea
+    descriptionEl.replaceWith(divEl);
+};
+
+// If you press either the description or the button, you get the same result, be that editing or finishing editing. 
+// TODO: Make the button work both to start and end editing. Don't know why it doesn't
+containerEl.on("click", "div.description", function() {
+    startEditing($(this));
+});
+containerEl.on("blur", "textarea.description", function() {
+    finishEditing($(this));
+});
+
+// containerEl.on("click", ".saveBtn", function() {
+//     const descriptionEl = $(this).siblings(".description");
+//     console.log(descriptionEl)
+//     $(this).trigger("blur");
+    
+//     if (descriptionEl.is("div")) {
+//         startEditing(descriptionEl);
+//     } else {
+//         finishEditing(descriptionEl);
+//     }
+// })
 
 displayDay();
 createHourBlocks();
